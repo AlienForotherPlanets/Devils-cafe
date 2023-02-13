129258c12ec1b1fc74598f90543b62b461cd8ef0
Keyur Vaghasiya <107289889+Keyur0016@users.noreply.github.com>
2023-02-13 18:10:38 +0530

    Upload files

---
+++ Style/AuthenticationStyle.js
@@ -0,0 +1,17 @@
+// **** Authentication screen style **** //
+
+let AuthenticationScreen_style = {
+    height: '100%', 
+    width: '100%', 
+    backgroundColor: 'white', 
+    paddingLeft: '4%', 
+    paddingRight: '4%', 
+    paddingTop: '2%'
+}
+
+let AuthenticationScreen_inputlayout = {
+    marginTop: "5%"
+}
+
+export {AuthenticationScreen_style, 
+        AuthenticationScreen_inputlayout} ; 
